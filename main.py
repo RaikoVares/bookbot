d02845ede662afa7ee3da7b861447b98b1ddab72
Raiko Vares <raiko_vares@hotmail.com>
2024-06-10 13:50:44 +0300

    added character_count func

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
     num_words = get_num_words(text)
+    character_count_dict = character_count(text)
     print(num_words)
 
 def get_book_text(file_path):
@@ -12,8 +13,17 @@ def get_book_text(file_path):
     
 def get_num_words(file_contents):
     words = file_contents.split()
-    word_count = len(words)
-    return word_count
+    return len(words)
     
+def character_count(text):
+    text = text.lower()
+    character_count_dict = {}
+    for letter in text:
+        if letter in character_count_dict.keys():
+            character_count_dict[letter] += 1
+        else:
+            character_count_dict[letter] = 1
+    print(character_count_dict)
+        
 
 main()
