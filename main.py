77262ce21a656e1e1c17efe36a1cd600a1653fd3
Vares <raiko_vares@hotmail.com>
2025-04-17 20:44:28 +0300

    code refactor, moved count words to stats.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-
+from stats import count_words
 
 def get_books_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
-
-def count_words(text):
-    word_lst = text.split()
-    return len(word_lst)
 
 def main():
     path = "books/frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(text):
+    word_lst = text.split()
+    return len(word_lst)
